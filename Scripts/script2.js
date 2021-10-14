04e8444006562110859c0d32261d8ddbbc0649a8
LinaSimon <90833662+LinaSimon@users.noreply.github.com>
2021-10-14 11:55:49 +0200

    made a clock

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,6 +1,35 @@
 var stageNr = 0;
+var changeNr = 1;
+var today;
+var time;
 
 function toggleState() {
-  document.querySelector("#kroppen").classList.toggle("stage_" + stageNr);
-  stageNr++;
+    document.getElementById("kroppen").classList = '';
+    document.getElementById("kroppen").classList.toggle("stage_" + stageNr);
+    stageNr += changeNr;
+    if(stageNr == 5) changeNr = -1;
+    if(stageNr == 0) changeNr = 1;
+    console.log(time);
+}
+
+
+$(document).ready(function(){
+  setTime();
+  setInterval(setTime, 1000);
+});
+function setTime(){
+    today = new Date();
+    //IF MINUTES AND SECONDS ARE DOUBLE DIGITS
+    if(today.getMinutes() > 9 && today.getSeconds() > 9)time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    //IF MINUTES IS NOT DOUBLE DIGITS
+    if(today.getMinutes() <= 9 && today.getSeconds() > 9)time = today.getHours() + ":0" + today.getMinutes() + ":" + today.getSeconds();
+
+    //IF SECONDS IS NOT DOUBLE DIGITS
+    if (today.getMinutes() > 9 && today.getSeconds() <= 9)time = today.getHours() + ":" + today.getMinutes() + ":0" + today.getSeconds();
+
+    //IF SECONDS AND MINUTES ARE NOT DOUBLE DIGITS
+    if (today.getMinutes() <= 9 && today.getSeconds() <= 9) time =today.getHours() +":0" +today.getMinutes() +":0" + today.getSeconds();
+
+    document.getElementById("date").innerHTML = time;
 }
