d56e218f98b3f13bd9ac5a1133cf0e17c00d533f
Anna Sakurai <zenasak@gmail.com>
2021-10-15 10:50:41 +0200

    fix stars on background

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -34,8 +34,8 @@ function setTime() {
 function backgroundColor() {
   let hr = new Date().getHours();
   let background = document.getElementById('kroppen');
-  let stars = document.getElementById('stars');
-  switch (hr) {
+  let stars = document.getElementById('particles-js');
+  switch (21) {
     default:
       background.classList.toggle('stage_0');
       stars.classList.toggle('noStars');
@@ -89,4 +89,73 @@ function backgroundColor() {
   }
 }
 
-function stars() {}
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 346, //この数値を変更すると星の数が増減できる
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: '#ffffff',
+    },
+    shape: {
+      type: 'circle', //形状はcircleを指定
+      stroke: {
+        width: 0,
+      },
+    },
+    opacity: {
+      value: 1, //シェイプの透明度
+      random: true, //シェイプの透明度をランダムにする
+      anim: {
+        enable: true, //シェイプの透明度をアニメーションさせる
+        speed: 3, //シェイプの透明度をアニメーションさせる
+        opacity_min: 0, //透明度の最小値０
+        sync: false, //全てを同時にアニメーションさせない
+      },
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.3,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 120, //この数値を小さくするとゆっくりな動きになる
+      direction: 'none', //方向指定なし
+      random: true, //動きはランダムに
+      straight: true, //動きをとどめる
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
