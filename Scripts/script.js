7f657f686c7eedd910184c23cb7124cc4c543893
Anna Sakurai <zenasak@gmail.com>
2021-10-19 13:46:49 +0200

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,12 +7,12 @@ var itemNr = 0;
 //JQUERY WHEN THE DOCUMENT LOADS WE CAN ADD ALL THE ELEMENTS WE WANT TO JAVASCRIPT
 $(document).ready(function () {
   //ADDING THE INPUT BOXES TO JAVASCRIPT
-  websiteDOM = document.getElementById("websiteDOM");
-  websiteShortName = document.getElementById("websiteShortname");
-  console.log(window.localStorage.getItem("DOM0"));
+  websiteDOM = document.getElementById('websiteDOM');
+  websiteShortName = document.getElementById('websiteShortname');
+  console.log(window.localStorage.getItem('DOM0'));
 
   //MODAL ELEMENT
-  modal = document.getElementById("myModal");
+  modal = document.getElementById('myModal');
 
   //RUN THE FUNCTION THAT LOADS PREVIOUSLY SAVED PAGES IN LOCAL STORAGE
   loadLocalWebpage();
@@ -26,7 +26,7 @@ function newWebPage() {
     shortname: websiteShortName.value,
   };
   //ADDING THE OBJECT TO LOCAL STORAGE, CHANING THE OBJECT TO A STRING THROUGH JSON.stringify
-  localStorage.setItem("website" + itemNr, JSON.stringify(website));
+  localStorage.setItem('website' + itemNr, JSON.stringify(website));
 
   //CALLING THE CREATE NEW DIV FUNCTION AND SENDING THE VALUES WRITTEN IN THE INPUT BOXES
   createNewDivWebPage(itemNr, websiteDOM.value, websiteShortName.value);
@@ -39,14 +39,14 @@ function newWebPage() {
 function createNewDivWebPage(nr, dom, shnm) {
   //DEFINING NEW DIV THROUGH JQUERY
   //(<div id='websiteBox(nr)' onClick="location.href'http://(domain name)'" class="websiteBoxClass" style="left:(100px*amount of boxes)">(shortname)</div>)
-  var $newDiv = $("<div/>");
-  $newDiv.attr("id", "websiteBox" + nr);
-  $newDiv.attr("onClick", `location.href='http://${dom}'`);
-  $newDiv.attr("class", "websiteBoxClass");
+  var $newDiv = $('<div/>');
+  $newDiv.attr('id', 'websiteBox' + nr);
+  $newDiv.attr('onClick', `location.href='http://${dom}'`);
+  $newDiv.attr('class', 'websiteBoxClass');
 
   //APPEND IMG TAG TO THE DIV TAG
   $newDiv.prepend(
-    `<button class="deleteBtn" onclick="deleteIcons(${nr})">X</button>
+    `<button class="deleteBtn" onclick="deleteIcons(${nr})" style="background-color: transparent; border: none; font-weight: 900; font-size: 1rem;">X</button>
         <div class="faviBox">
             <img src="http://www.google.com/s2/favicons?domain=${dom}"
             alt="favicon" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);height:50%;width:50%;"/>
@@ -54,7 +54,7 @@ function createNewDivWebPage(nr, dom, shnm) {
         <div class="shortNameBox">${shnm}</div>`
   );
 
-  $newDiv.appendTo("#favWebBox").html();
+  $newDiv.appendTo('#favWebBox').html();
 }
 
 //LOAD ALL THE PAGES SAVED IN LOCAL STORAGE
@@ -68,7 +68,7 @@ function loadLocalWebpage() {
 
     //CREATING NEW DIVS FOR EACH LOCAL STORAGE ITEM
     createNewDivWebPage(
-      Object.keys(localStorage)[i].replace("website", ""),
+      Object.keys(localStorage)[i].replace('website', ''),
       divAttr.dom,
       divAttr.shortname
     );
@@ -78,30 +78,30 @@ function loadLocalWebpage() {
   }
 }
 
-const deleteBtn = document.querySelectorAll(".deleteBtn");
+const deleteBtn = document.querySelectorAll('.deleteBtn');
 console.log(deleteBtn);
 
 //REMOVE ICONS ON PRESSING THE "X" BUTTON
 function deleteIcons(nr) {
-  window.localStorage.removeItem("website" + nr);
-  document.getElementById("websiteBox" + nr).remove();
+  window.localStorage.removeItem('website' + nr);
+  document.getElementById('websiteBox' + nr).remove();
   event.stopPropagation();
 }
 
 // MODAL
 // USER CLICKS ON BUTTON OPEN MODAL
 function showModal() {
-  modal.style.display = "block";
+  modal.style.display = 'block';
 }
 
 // CLOSE THE MODAL WHEN USER CLICKS ON SPAN
 function hideModal() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
 // CLOSES THE WINDOW WHEN USER CLICKS OUTSIDE THE MODAL
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 };
